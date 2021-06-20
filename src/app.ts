18498e0a670f900882aa541cf9c2de6a73e296c5
Hyxotis <chris.chich@hotmail.fr>
2021-06-20 17:01:15 +0200

    :sparkles: Added service discovery

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import FetchServiceRequest from "./types/requests/fetchServiceRequest";
 import fetchService from "./use_cases/fetchService";
+import FetchError from "./types/errors/fetchError";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(cookieParser());
@@ -9,6 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(async (req, res) => {
+  if (!req.path.startsWith("/api/") || req.path.length < 5) {
+    return res.status(400).send("Route should start with /api/");
+  }
+
   const fetchServiceRequest: FetchServiceRequest = {
     path: req.path,
     headers: req.headers,
@@ -17,10 +25,16 @@ app.use(async (req, res) => {
   };
 
   try {
-    res.send(await fetchService(fetchServiceRequest));
-  } catch (error) {}
+    return res.send(await fetchService(fetchServiceRequest));
+  } catch (error) {
+    const fetchError: FetchError = error;
+    return res.status(fetchError.code).send({
+      message: fetchError.message,
+      details: fetchError.details,
+    });
+  }
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT ? process.env.port : 3000, () => {
   console.log("The application is listening on port 3000!");
 });
--- src/use_cases/fetchService.ts
+++ src/use_cases/fetchService.ts
@@ -1,5 +1,20 @@
 import FetchServiceRequest from "../types/requests/fetchServiceRequest";
+import ServiceNotFoundError from "../types/errors/serviceNotFoundError";
 
 export default async (request: FetchServiceRequest) => {
+  const service = request.path.split("/")[2];
+
+  if (!service) {
+    throw new ServiceNotFoundError();
+  }
+
+  let serviceRoles = process.env[`GATEWAY_ROUTE_${service.toUpperCase()}`];
+
+  if (!serviceRoles) {
+    throw new ServiceNotFoundError();
+  }
+
+  serviceRoles = JSON.parse(serviceRoles);
+
   return request;
 };
